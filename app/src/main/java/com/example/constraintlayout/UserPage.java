55fb7778ec606c245cd6c615b246987802d64081
muhammet şeramet <muhammetseramet@marun.edu.tr>
2019-04-21 06:31:35 +0300

    some changes

--- app/src/main/java/com/example/constraintlayout/UserPage.java
+++ app/src/main/java/com/example/constraintlayout/UserPage.java
@@ -3,10 +3,9 @@ package com.example.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class UserPage extends AppCompatActivity {
 
     public TextView user_info;
     public TextView fav_rest;
@@ -15,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user_page);
 
         String restNames[] = getRestaurants();
 
